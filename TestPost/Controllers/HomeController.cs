760d0f748a5636c1f1735268ae721522ce8723b7
adi555j <adi555j@gmail.com>
2021-10-06 03:19:34 +0530

    Add project files.

---
+++ TestPost/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestPost.Controllers
+{
+    public class HomeController : Controller
+    {
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public System.Web.Mvc.ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public System.Web.Mvc.ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public System.Web.Mvc.ActionResult Index([FromBody]Values values)
+        {
+            return View();
+        }
+    }
+
+    public class Values
+    {
+        public string test1 { get; set; }
+        public string test2 { get; set; }
+    }
+}
